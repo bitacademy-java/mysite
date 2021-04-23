f665f7fbed46411e1fa3f51f5219296107fd6fa8
'bitacademy-java' <'java.bitacademy@gmail.com'>
2021-04-23 12:50:36 +0900

    ajax #finish

--- mysite03/src/main/java/com/bitacademy/mysite/controller/api/UserController.java
+++ mysite03/src/main/java/com/bitacademy/mysite/controller/api/UserController.java
@@ -1,34 +1,25 @@
 package com.bitacademy.mysite.controller.api;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
+import com.bitacademy.dto.JsonResult;
 import com.bitacademy.mysite.service.UserService;
 
 @Controller("userApiController")
 @RequestMapping("/api/user")
 public class UserController {
-	
 	@Autowired
 	private UserService userService;
 	
 	@ResponseBody
 	@RequestMapping("/existemail")
-	public Map<String, Object> existEmail(String email){
+	public JsonResult existEmail(String email){
 		Boolean result = userService.existUser(email);
-		
-		Map<String, Object> map = new HashMap<>();
-		map.put("result", "success");
-		map.put("data", result);
-		return map;
+		return JsonResult.success(result);
 	}
-	
 	
 	@ResponseBody
 	@RequestMapping("/hello")
